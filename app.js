b99097f1f9b694e64adff74c48cf0e6503b240cd
Jen <jenyee1022@yahoo.com>
2017-07-08 21:11:01 -0700

    added animation to player, not scaled

--- app.js
+++ app.js
@@ -8,11 +8,27 @@
 
 
 $(document).ready(() => {
-
   var $rink = $("<div class='rink'></div>");
   var $blueLines = $("<div class='blueLines'></div>");
-  var $player = $("<div class='player></div>'");
+  var $player = $("<div class='player'></div>'");
+  var $button = $("<button class='my-button'>Start</button>")
   $rink.append($blueLines);
   $(".entry").append($player);
   $(".entry").append($rink);
+  $(".entry").append($button);
+  $.get( "http://localhost:8080/data", function( data ) {
+    var first5Points = data['2KLH-JA72'].slice(0, 5);
+    var startingPos = [`${first5Points[0][2]}px`, `${first5Points[0][3]}px`]
+    $(".player").css({"top": startingPos[0], "left": startingPos[1]});
+    $(".my-button").click(function() {
+    	var coords = first5Points.slice(1).map(val => [val[2], val[3]]);
+      console.log(coords)
+      coords.forEach(function(xy) {
+      	$(".player").animate({
+      		left: "+=" + xy[0],
+        	bottom: "+=" + xy[1]
+      	}, 2000, function() {})
+      })
+    })
+  });
 });
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 var data = require('./data.json');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/data", (req, res) => {
   res.json(data);
 });
