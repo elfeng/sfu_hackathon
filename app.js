7f1344a95f3eb1a88c3655988c47797773cf2146
Jen <jenyee1022@yahoo.com>
2017-07-09 11:10:53 -0700

    animation complete

--- app.js
+++ app.js
@@ -6,6 +6,10 @@
 
 // sample ------ var $tweet = $("<article>").addClass("tweet");  --------
 
+// failed drop example - entry 5
+// successful drop example - entry 15
+// failed dump example -
+
 
 $(document).ready(() => {
   var $rink = $("<div class='rink'></div>");
@@ -16,17 +20,33 @@ $(document).ready(() => {
   $(".entry").append($player);
   $(".entry").append($rink);
   $(".entry").append($button);
-  $.get( "http://localhost:8080/data", function( data ) {
-    var first5Points = data['2KLH-JA72'].slice(0, 5);
-    var startingPos = [`${first5Points[0][2]}px`, `${first5Points[0][3]}px`]
-    $(".player").css({"top": startingPos[0], "left": startingPos[1]});
+  $.get( "http://localhost:8080/attempts", function( data ) {
+    var attemptNumber = 39;
+    var $playType = $(`<p>${data[attemptNumber][`Entry ${attemptNumber}`].style.toUpperCase()} type entry</p>
+    `);
+    var $success = $(`<p>Successful: ${data[attemptNumber][`Entry ${attemptNumber}`].success.toString()}</p>`)
+    $(".textbox").append($playType);
+    $(".textbox").append($success);
+    var coordinates = data[attemptNumber][`Entry ${attemptNumber}`]['coords'].map(xy => [xy[0]*4, xy[1]*4]);
+    console.log(coordinates)
+    var origin = [-4, 4]
+    var startingPos = [
+      `${(coordinates[0][0] + origin[0]).toString()}px`,
+      `${(coordinates[0][1] + origin[1]).toString()}px`
+    ];
+    $(".player").css({"left": startingPos[0], "top": startingPos[1]});
+
     $(".my-button").click(function() {
-    	var coords = first5Points.slice(1).map(val => [val[2], val[3]]);
-      console.log(coords)
-      coords.forEach(function(xy) {
+      // find the difference in xy between each point
+    	var movements = coordinates.slice(1).map((val, index, array) => {
+        var deltaX = val[0] - coordinates[index][0];
+        var deltaY = val[1] - coordinates[index][1];
+        return [deltaX, deltaY]
+      });
+      movements.forEach(function(xy) {
       	$(".player").animate({
       		left: "+=" + xy[0],
-        	bottom: "+=" + xy[1]
+        	top: "+=" + xy[1]
       	}, 2000, function() {})
       })
     })
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 var data = require('./data.json');
+var attempts = require('./attempts2.json');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,6 +12,10 @@ app.use(function(req, res, next) {
 
 app.get("/data", (req, res) => {
   res.json(data);
+});
+
+app.get("/attempts", (req, res) => {
+  res.json(attempts);
 });
 
 app.listen(PORT, () => {
